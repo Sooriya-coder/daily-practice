bb27b90d943ed7b1eb6a9c948c04a7970bc5209c
Sooriya-coder <sooriyapranyah@gmail.com>
2026-07-03 21:12:10 +0530

    understanding the concepts of list

---
+++ list_1.py
@@ -0,0 +1,13 @@
+#list can be declared oro denoted by square brackets[]
+#list is ordered 
+#list is mutable 
+#we can add any kind of data type in list
+#we can add list inside the list
+
+ 
+family_members=["sooriya","sundari","pranyah","susi"]
+print(family_members)
+print(len(family_members))
+
+smart=["sooriya",23,7868885059,True,False,23.9]
+print(smart)

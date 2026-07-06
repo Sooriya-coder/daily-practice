4b5cded6dff45b8a72ecfda5b66b2acf11966c39
Sooriya-coder <sooriyapranyah@gmail.com>
2026-07-06 06:32:25 +0530

    practicing list indexing and slicing

---
+++ list_index.py
@@ -0,0 +1,19 @@
+#understanding the indexing concept in list 
+list=["virat","salt","padikal","rajat","jithesh sharma","tim","krunal","sheperd"]
+
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[5])
+print(list[6])
+print(list[7])
+
+print(list[-1])
+print(list[-2])
+print(list[-3])
+print(list[-4])
+print(list[-5])
+print(list[-6])
+print(list[-7])
---
+++ list_slice.py
@@ -0,0 +1,13 @@
+# understanding the concept of list slicing concept
+#start,stop,step
+
+list=["virat","salt","padikal","rajat","jithesh sharma","tim","krunal","sheperd"]
+
+print(list[0:])
+print(list[:7])
+print(list[0:5])
+print(list[3:7])
+print(list[0:7:2])
+print(list[0:7:3])
+print(list[0:7:4])
+print(list[::-1])
